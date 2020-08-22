ecdb2b1f8fec25a5ff077c63bd32107e9e54450f
Sarah Heanan <sarahlheanan@gmail.com>
2020-08-22 14:00:05 +0100

    up to episode 85

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,7 +1,57 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
     
+    //function constructors
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: [],
+        },
+        totals: {
+            exp: 0,
+            inc: 0,
+        }
+    }
+
+    return {
+        addItem: function(type, descr, value) {
+            var newItem, ID;
+
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if(type === 'exp') {
+                newItem = new Expense(ID, descr, value);
+            } else if(type === 'inc') {
+                newItem = new Income(ID, descr, value);
+            }
+
+            data.allItems[type].push(newItem);
+
+            return newItem;
+ 
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
     
 })();
 
@@ -33,6 +83,39 @@ var UIController = (function() {
             };
         },
 
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            if(type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //all html inserted as last child of containers
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
 
         getDOMstrings: function() {
             return DOMstrings;
@@ -59,8 +142,14 @@ var controller = (function(bugetCtrl, UICtrl) {
     }
 
     var ctrlAddItem = function() {
-        var input = UICtrl.getInput();
-        console.log(input);
+        var input, newItem;
+        input = UICtrl.getInput();
+        
+        newItem = budgetController.addItem(input.type, input.description, input.value);
+
+        UICtrl.addListItem(newItem, input.type);
+
+        UICtrl.clearFields();
     };
 
     return {
